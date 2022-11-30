acd5e77cbbc16176a057eb8a2cd6d6d6fd3cd4d1
Kruthika R <kruthika.r@publicissapient.com>
2022-11-30 08:41:41 +0530

    Assignment1

---
+++ src/test/java/assignment/ConcatAssignment.java
@@ -0,0 +1,14 @@
+package assignment;
+
+import org.junit.Test;
+
+public class ConcatAssignment 
+{
+  @Test
+  public void concat()
+  {
+	  String firstname="Kruthika";
+	  String lastname="Rupesh";
+	  System.out.println("Hello" +" " +firstname +" " +lastname);
+  }
+}
